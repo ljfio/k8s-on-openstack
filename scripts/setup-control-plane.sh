59122041da467413f53b3031caf93c3fd489bbae
Luke Fisher <mail@lukefisher.me>
2022-01-27 23:09:54 +0000

    Update control plane setup script

--- scripts/setup-control-plane.sh
+++ scripts/setup-control-plane.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-kubeadm init
+curl -L -O https://docs.projectcalico.org/manifests/calico.yaml
+
+kubeadm config print init-defaults | tee ClusterConfiguration.yaml
+
+dasel put string -p yaml -f ClusterConfiguration.yaml -s '[0].localAPIEndpoint.advertiseAddress' $(hostname -I)
+
+dasel put string -p yaml -f ClusterConfiguration.yaml -s '[0].nodeRegistration.criSocket' $(hostname)
+
+dasel put string -p yaml -f ClusterConfiguration.yaml -s '[0].nodeRegistration.criSocket' '/var/run/containerd/containerd.sock'
+
+cat <<EOF | tee -a ClusterConfiguration.yaml
+---
+apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+cgroupDriver: systemd
+EOF
+
+kubeadm init --config ClusterConfiguration.yaml --cri-socket /var/run/containerd/containerd.sock
 
 mkdir -p $HOME/.kube
 
@@ -7,3 +24,4 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 
 chown $(id -u):$(id -g) $HOME/.kube/config
 
+kubectl apply -f calico.yaml
